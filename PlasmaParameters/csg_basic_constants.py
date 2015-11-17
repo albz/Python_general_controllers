4f14d412fa63cb1fe25c181b6b0e89069b64eb93
Alberto Albz Marocchino <albz.uk@gmail.com>
2015-11-17 22:21:05 +0100

    new parameters in plasma_basic_parameters

---
+++ PlasmaParameters/csg_basic_constants.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+######################################################################
+# Name:         PlasmaParameters
+# Author:       A. Marocchino
+# Date:			2015-11-17
+# Purpose:      some useful cgs constant used in: PlasmaParameters
+# Source:       python
+#####################################################################
+
+#--- some cgs physical constants ---#
+electron_mass_cgs 		= 9.1e-28 #[g]
+electron_charge_csg		= 4.80320e-10; #[StatC]
+proton_mass_cgs			= 1.660538921e-24 #[g] 
+c_cgs					= 29979245800. #[cm] 
+boltzmann_constant_ergK	= 1.38064e-16 #[erg/K]
+#--- *** ---#
