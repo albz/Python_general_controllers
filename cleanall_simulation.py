6e2396592f111a78a7f59917eee95a9882f5dd1f
Alberto Albz Marocchino <albz.uk@gmail.com>
2016-04-18 09:44:33 +0200

    add file extensions

--- cleanall_simulation.py
+++ cleanall_simulation.py
@@ -17,10 +17,10 @@ import os, os.path, sys, shutil, time, string
 path = os.getcwd()
 
 #namelist files
-nml_file = ['dued.nml','dued_1D.nml','dued.nml_1D','dued_2D.nml','dued.nml_2D','architect.nml'];
+nml_file = ['dued.nml','dued_1D.nml','dued.nml_1D','dued_2D.nml','dued.nml_2D','architect.nml','makefile','Makefile'];
 
 #Exclusion List
-Exc_list = ['.nml','nml_1D','nml_2D','.f','.f90','.c','.cpp','.py','.inp'];
+Exc_list = ['.nml','nml_1D','nml_2D','.f','.f90','.c','.cpp','.py','.inp','.m'];
 
 
 #Search and Delete
@@ -31,12 +31,8 @@ for root_dir, sub_dirs, files in os.walk(path):
 			nml_exists += 1
 		if file not in nml_file and os.path.splitext(file)[1] not in Exc_list:
 			os.remove(os.path.join(root_dir,file))
-			
+
 	if nml_exists > 0 and os.path.exists(os.path.join(root_dir,'out')) == True:
  		shutil.rmtree(os.path.join(root_dir,'out'))
 	if nml_exists > 0 and os.path.exists(os.path.join(root_dir,'dump')) == True:
  		shutil.rmtree(os.path.join(root_dir,'dump'))
-	
-		
-	
-				
