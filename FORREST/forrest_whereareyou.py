aae676ea431fd921dba06de2348f84b4335e70f4
Alberto Albz Marocchino <albz.uk@gmail.com>
2016-11-07 08:23:50 +0100

    correction to 'whereareyou' to display architect run distance in microns

--- FORREST/forrest_whereareyou.py
+++ FORREST/forrest_whereareyou.py
@@ -33,7 +33,7 @@ class run_status:
 		self.time.append(time)
 		self.dt.append(dt)
 		self.status.append(status)
-		
+
 	def print_whole_status(self):
 		print self.identifier
 		print self.program
@@ -41,7 +41,7 @@ class run_status:
 		print self.time
 		print self.dt
 		print self.status
-		
+
 	def count_run(self,program,status):
 		nsim=0
 		for id in self.identifier:
@@ -49,35 +49,53 @@ class run_status:
 				nsim += 1
 		return nsim
 
-	def print_status(self,program,status):
-		print program,' : ', status , ' : >>>', self.count_run(program,status)
+	def count_run_allstatus(self,program):
+		nsim=0
 		for id in self.identifier:
-			if self.program[id] == program and self.status[id] == status:
-				print self.path[id], '--- lastest sim time: %2.4f (ns) --- dt=%2.4e' % (self.time[id],self.dt[id])
-	
+			if self.program[id] == program:
+				nsim += 1
+		return nsim
+
+	def print_status(self,program,status):
+		if( self.count_run_allstatus(program) > 0):
+			print program,' : ', status , ' : >>>', self.count_run(program,status)
+			for id in self.identifier:
+				if self.program[id] == program and self.status[id] == status and program == 'dued':
+					print self.path[id], '--- lastest sim time: %2.4f (ns) --- dt=%2.4e' % (self.time[id],self.dt[id])
+				if self.program[id] == program and self.status[id] == status and program == 'architect':
+					print self.path[id], '--- lastest sim distance: %2.1f (um)' % (self.time[id])
+
 	def print_general_status(self,program):
-		print '*** *** ***'
-		print "%-8s %-3s %-23s %4d" % (program,">>>","Simulations run so far:",self.count_run(program,'==completed=='))
-		print "%-8s %-3s %-23s %4d" % (program,">>>","Simulations running:",self.count_run(program,'==started=='))
-		print "%-8s %-3s %-23s %4d" % (program,">>>","Simulations to run:",self.count_run(program,'to_be_run'))
-		print "%-8s %-3s %-23s %4d" % (program,">>>","Simulations suspended:",self.count_run(program,'==suspended=='))
-		print "%-8s %-3s %-23s %4d" % (program,">>>","Simulations failed:",self.count_run(program,'==failed=='))
-		print "%-8s %-3s %-23s %4d" % (program,">>>","Zombie Simulations:",self.count_run(program,'==zombie=='))
-		print '*** *** ***'
+		if( self.count_run_allstatus(program) > 0):
+			print '*** *** ***'
+			print "%-8s %-3s %-23s %4d" % (program,">>>","Simulations run so far:",self.count_run(program,'==completed=='))
+			print "%-8s %-3s %-23s %4d" % (program,">>>","Simulations running:",self.count_run(program,'==started=='))
+			print "%-8s %-3s %-23s %4d" % (program,">>>","Simulations to run:",self.count_run(program,'to_be_run'))
+			print "%-8s %-3s %-23s %4d" % (program,">>>","Simulations suspended:",self.count_run(program,'==suspended=='))
+			print "%-8s %-3s %-23s %4d" % (program,">>>","Simulations failed:",self.count_run(program,'==failed=='))
+			print "%-8s %-3s %-23s %4d" % (program,">>>","Zombie Simulations:",self.count_run(program,'==zombie=='))
+			print '*** *** ***'
 
 #--- *** ---#
-def find_running_time(path):
-	try:
-		return float(open(os.path.join(path,'cpu'), 'r').readlines()[-1].split()[1])*1e9
-	except:
-		return 0.0 
+def find_running_time(path,program):
+	if program == 'dued.nml':
+		try:
+			return float(open(os.path.join(path,'cpu'), 'r').readlines()[-1].split()[1])*1e9
+		except:
+			return 0.0
+	if program == 'architect':
+		try:
+			return -1.*float(open(os.path.join(path,'sim.out'), 'r').readlines()[-1].split()[7])
+		except:
+			return 0.0
+
 
 #--- *** ---#
 def find_last_dt(path):
 	try:
 		return float(open(os.path.join(path,'cpu'), 'r').readlines()[-1].split()[1]) - float(open(os.path.join(path,'cpu'), 'r').readlines()[-2].split()[1])
 	except:
-		return 0.0 
+		return 0.0
 
 #--- *** ---#
 def get_flag(path):
@@ -101,7 +119,7 @@ def get_flag(path):
 			return 'to_be_run'
 
 
-#--- *** ---#	
+#--- *** ---#
 def prune_dirs(dirs):
 	if 'out' in dirs:
 		dirs.remove('out')
@@ -120,14 +138,14 @@ counter = 0
 for root_dir, sub_dirs, files in os.walk(path):
 	prune_dirs(sub_dirs)
 	if 'dued.nml' in files:
-		time = find_running_time(root_dir)
+		time = find_running_time(root_dir,'dued')
 		dt   = find_last_dt(root_dir)
 		flag = get_flag(root_dir)
 		runs.new_sim_status(counter,'dued',root_dir,time,dt,flag)
 		counter += 1
-		
+
 	if 'architect.nml' in files:
-		time = find_running_time(root_dir)
+		time = find_running_time(root_dir,'architect')
 		dt   = find_last_dt(root_dir)
 		flag = get_flag(root_dir)
 		runs.new_sim_status(counter,'architect',root_dir,time,dt,flag)
@@ -145,5 +163,3 @@ runs.print_status('architect','==started==')
 runs.print_status('architect','==failed==')
 runs.print_status('architect','==zombie==')
 runs.print_status('architect','==completed==')
-
-	
